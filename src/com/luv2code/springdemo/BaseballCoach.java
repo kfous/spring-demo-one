7190bc709ea42109895e859d83c8ad7af0b9962e
FOUSEKIS Konstantinos <konstantinos.fousekis@sword-group.com>
2021-03-31 13:28:49 +0300

    First commit

---
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach{
+
+    // define a private field for the dependency
+    private FortuneService fortuneService;
+
+    //CONSTRUCTOR DEPENDENCY INJECTION
+    public BaseballCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "spend 30minutes on batting practice";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        // use my fortune service to get a fortune
+        return fortuneService.getFortune();
+    }
+}
