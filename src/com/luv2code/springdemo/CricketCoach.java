7eb9279a86b3b33c2049dbf4fc9fadd9f02a13b2
FOUSEKIS Konstantinos <konstantinos.fousekis@sword-group.com>
2021-03-31 15:02:06 +0300

    First injection of literal values

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -4,13 +4,37 @@ public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    // email and team fields
+    private String emailAddress;
+    private String team;
+
     public CricketCoach( ) {
         System.out.println("CricketCoach: inside no-arg constructor");
     }
 
     public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("CricketCoach: setter method");
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach:inside setter method - setEmailAddress");
+
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+
+        this.team = team;
     }
 
     @Override
